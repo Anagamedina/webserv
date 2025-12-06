5a9ce2b0576f3ea9098290fe4c8f3480e1145382
cpujades <cpujades@student.42barcelona.com>
2025-12-06 19:29:29 +0100

    add compile commands, edit cmake to obtain .cpp

---
+++ INFO.md
@@ -0,0 +1,8 @@
+# Compilation flags for makefile
+
+## Generate Compile Commands
+```sh
+cd <build_dir>
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON .. # this generates compile_commands.json
+```
+
--- src/network/TcpListener.cpp
+++ src/network/TcpListener.cpp
@@ -37,7 +37,7 @@ void TcpListener::setSocketOptions() {
         close(socket_fd_);
         throw std::runtime_error("Failed to set socket options");
     }
-    
+
     // Set socket to non-blocking mode
     int flags = fcntl(socket_fd_, F_GETFL, 0);
     if (flags == -1) {
@@ -53,11 +53,11 @@ void TcpListener::setSocketOptions() {
 void TcpListener::bindSocket() {
     struct sockaddr_in address;
     std::memset(&address, 0, sizeof(address));
-    
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;  // Listen on all interfaces
     address.sin_port = htons(port_);
-    
+
     if (bind(socket_fd_, (struct sockaddr*)&address, sizeof(address)) < 0) {
         close(socket_fd_);
         throw std::runtime_error("Failed to bind socket to port " + string_utils::toString(port_));
@@ -76,7 +76,7 @@ void TcpListener::listen() {
 int TcpListener::acceptConnection() {
     struct sockaddr_in client_addr;
     socklen_t addr_len = sizeof(client_addr);
-    
+
     int client_fd = accept(socket_fd_, (struct sockaddr*)&client_addr, &addr_len);
     if (client_fd < 0) {
         if (errno != EWOULDBLOCK && errno != EAGAIN) {
@@ -84,18 +84,18 @@ int TcpListener::acceptConnection() {
         }
         return -1;
     }
-    
+
     // Set client socket to non-blocking
     int flags = fcntl(client_fd, F_GETFL, 0);
     if (flags != -1) {
         fcntl(client_fd, F_SETFL, flags | O_NONBLOCK);
     }
-    
+
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
     std::cout << "New connection from " << client_ip << ":" << ntohs(client_addr.sin_port)
               << " (fd: " << client_fd << ")" << std::endl;
-    
+
     return client_fd;
 }
 
