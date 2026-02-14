1ff587e4d974f6c08d57e37aea8fc706dd8f984c
Daruuu <thedaruny16@gmail.com>
2026-02-14 19:53:09 +0100

    [DARU-TEST-CONFIG] try to find errors in config file validation and store in config/TODO.txt

--- src/common/namespaces.hpp
+++ src/common/namespaces.hpp
@@ -122,7 +122,7 @@ namespace config
 		static const std::string preferential_prefix_modifier = "^~";
 
 		static const int default_return_code = 302;
-		static const int default_port = 80;
+		static const int default_port = 8080;
 		static const std::string default_host_name = "127.0.0.1";
 		static const size_t max_body_size = 1048576;
 		static const int max_port = 65535;
