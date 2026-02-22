5178aab8f316a98ff81da9925ee0b20d9fdebbf3
Daruuu <thedaruny16@gmail.com>
2026-02-22 21:05:48 +0100

    [DARU-SERVER] update documentation

--- src/config/ServerConfig.hpp
+++ src/config/ServerConfig.hpp
@@ -6,20 +6,27 @@
 #include <string>
 #include <vector>
 
-#include "../common/namespaces.hpp"
 #include "LocationConfig.hpp"
 
 /**
- * ServerConfig stores configuration for one server { } block
- * Corresponds to nginx-like config:
+ * @brief Configuration of one server { } block
+ * Represents a virtual server (nginx-like style).
+ *
+ * Example equivalent configuration:
+ * ```
  * server {
- *     listen 8080;
- *     host 127.0.0.1;
- *     server_name example.com;
- *     max_body_size 1048576 (bytes);
- *     error_page 404 /404.html;
+ *     listen          8080;
+ *     host            127.0.0.1;
+ *     server_name     example.com;
+ *     root            /var/www/html;
+ *     index           index.html index.htm;
+ *     max_body_size   1048576;
+ *     error_page      404 /errors/404.html;
+ *     autoindex       on;
+ *     return          301 http://example.com;
  *     location / { ... }
  * }
+ * ```
  */
 class ServerConfig {
  public:
@@ -60,7 +67,7 @@ class ServerConfig {
   const std::string& getRedirectUrl() const;
   size_t getGlobalMaxBodySize() const;
 
-  // Debug
+  // Debug Helper
   void print() const;
 
  private:
@@ -69,7 +76,7 @@ class ServerConfig {
   std::string server_name_;
   std::string root_;
   std::vector<std::string> indexes_;
-  size_t max_body_size_;  // max is 1048576 (bytes)
+  size_t max_body_size_;
   int cgi_timeout_;
   std::map<int, std::string> error_pages_;
   std::vector<LocationConfig> locations_;
@@ -78,90 +85,6 @@ class ServerConfig {
   std::string redirect_url_;
 };
 
-inline std::ostream& operator<<(std::ostream& os, const ServerConfig& config) {
-  os << config::colors::blue << config::colors::bold << "Server Config:\n"
-     << config::colors::reset << "\t" << config::colors::yellow
-     << "Port: " << config::colors::reset << config::colors::green
-     << config.getPort() << config::colors::reset << "\n"
-     << "\t" << config::colors::yellow << "Host: " << config::colors::reset
-     << config::colors::green << config.getHost() << config::colors::reset
-     << "\n"
-     << "\t" << config::colors::yellow
-     << "Server name: " << config::colors::reset << config::colors::green
-     << config.getServerName() << config::colors::reset << "\n"<< config::colors::yellow
-	 << "\tMax body size: " << config::colors::reset << config::colors::green
-	 << config.getMaxBodySize() << config::colors::reset << "\n";
-
-  const ServerConfig::ErrorMap& errorPages = config.getErrorPages();
-  os << "\t" << config::colors::yellow << "Error pages:\n"
-     << config::colors::reset;
-  if (!errorPages.empty()) {
-    std::map<std::string, std::vector<int> > groupedErrors;
-    for (ServerConfig::ErrorIterator it = errorPages.begin();
-         it != errorPages.end(); ++it) {
-      groupedErrors[it->second].push_back(it->first);
-    }
-    for (std::map<std::string, std::vector<int> >::const_iterator groupIt =
-             groupedErrors.begin();
-         groupIt != groupedErrors.end(); ++groupIt) {
-      os << "\t";
-      const std::vector<int>& codes = groupIt->second;
-      for (std::vector<int>::const_iterator codeIt = codes.begin();
-           codeIt != codes.end(); ++codeIt) {
-        os << config::colors::magenta << " " << *codeIt
-           << config::colors::reset;
-      }
-      os << config::colors::green << " " << groupIt->first
-         << config::colors::reset << "\n";
-    }
-  } else {
-    os << config::colors::red << "\t\tNot configured"
-       << config::colors::reset << "\n";
-  }
-
-  const std::vector<LocationConfig>& locations = config.getLocations();
-  for (size_t i = 0; i < locations.size(); ++i) {
-    os << locations[i];
-  }
-  return os;
-}
-
-/*
-inline std::ostream& operator<<(std::ostream& os, const ServerConfig& config) {
-    os << config::colors::blue << "Server Config:\n"
-       << config::colors::reset << "\tPort: " << config.getPort()
-       << "\n\tHost: " << config.getHost() << "\n\tServer name: " <<
-config.getServerName() << "\n";
-
-    const ServerConfig::ErrorMap& errorPages = config.getErrorPages();
-    os << "\tError pages:\n";
-
-    if (!errorPages.empty()) {
-        std::map< std::string, std::vector< int > > groupedErrors;
-
-        for (ServerConfig::ErrorIterator it = errorPages.begin(); it !=
-errorPages.end(); ++it) { groupedErrors[it->second].push_back(it->first);
-        }
-
-        for (std::map< std::string, std::vector< int > >::const_iterator groupIt
-= groupedErrors.begin(); groupIt != groupedErrors.end(); ++groupIt) { os <<
-"\t"; const std::vector< int >& codes = groupIt->second; for (std::vector< int
->::const_iterator codeIt = codes.begin(); codeIt != codes.end();
-                 ++codeIt) {
-                os << " " << *codeIt;
-            }
-            os << " " << groupIt->first << "\n";
-        }
-    } else {
-        os << "\tNot configured\n";
-    }
-
-    const std::vector< LocationConfig >& locations = config.getLocations();
-    for (size_t i = 0; i < locations.size(); ++i) {
-        os << locations[i];
-    }
-    return os;
-}
-*/
+std::ostream& operator<<(std::ostream& os, const ServerConfig& config);
 
 #endif  // WEBSERV_SERVERCONFIG_HPP
